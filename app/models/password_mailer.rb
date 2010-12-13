48020a43fd40c7e154e66701632c2bf67aa1a5a1
Eric Anderson <eric@pixelwareinc.com>
2010-12-13 12:23:44 -0500

    Use domain instead of host

--- app/models/password_mailer.rb
+++ app/models/password_mailer.rb
@@ -3,7 +3,7 @@ class PasswordMailer < ActionMailer::Base
   def password_reset_instructions(authenticated_record, request)
     @authenticated_record = authenticated_record
     mail :to => authenticated_record.email,
-      :from => "no-reply@#{request.host}",
+      :from => "no-reply@#{request.domain}",
       :subject => "[#{request.host}] Password Reset"
   end
 
