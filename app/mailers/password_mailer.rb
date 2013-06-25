02e32a0b35c2f53f9669a208e64672b9be96c3ae
Eric Anderson <eric@pixelwareinc.com>
2013-06-25 19:41:47 -0400

    Allow from e-mail to be fully customized.
    Allow domain to be hard-coded instead of automatically assumed from
    the request. By default it is still automatically assumed from the request.

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,12 +1,19 @@
 class PasswordMailer < ActionMailer::Base
+  # The user this password reminder should appear to be coming from.
+  # Default to 'no-reply@websitedomain.com'. The domain part is
+  # automatically determined unless the domain class accessor is set.
   cattr_accessor :from_user
   self.from_user = 'no-reply'
 
+  # Automatically set to the domain of the web request if not set
+  cattr_accessor :domain
+
   def password_reset_instructions(authenticated_record, request)
     @authenticated_record = authenticated_record
+    domain = self.class.domain || request.domain
     mail :to => authenticated_record.email,
-      :from => "#{self.class.from_user}@#{request.domain}",
-      :subject => "[#{request.host}] Password Reset"
+      :from => "#{self.class.from_user}@#{domain}",
+      :subject => "[#{domain}] Password Reset"
   end
 
 end
--- test/unit/password_mailer_test.rb
+++ test/unit/password_mailer_test.rb
@@ -12,10 +12,52 @@ class PasswordMailerTest < ActionMailer::TestCase
 
     assert_equal ['no-reply@domain.com'], email.from
     assert_equal ['joe@example.com'], email.to
-    assert_equal "[www.domain.com] Password Reset", email.subject
+    assert_equal "[domain.com] Password Reset", email.subject
 
     reset_url = "http://www.domain.com/passwords/#{user.perishable_token}/edit"
     assert_match reset_url, email.body.encoded
+  end
+
+  def test_alternate_user
+    user = create_user
+    request = ActionDispatch::TestRequest.new
+    request.host = 'www.domain.com'
+
+    PasswordMailer.from_user = 'me'
+    PasswordMailer.default_url_options[:host] = request.host
+    email = PasswordMailer.password_reset_instructions user, request
+
+    assert_equal ['me@domain.com'], email.from
+  ensure
+    PasswordMailer.from_user = 'no-reply'
+  end
+
+  def test_alternate_domain
+    user = create_user
+
+    PasswordMailer.domain = 'another.com'
+    PasswordMailer.default_url_options[:host] = 'example.com'
+    email = PasswordMailer.password_reset_instructions user, nil
+
+    assert_equal ['no-reply@another.com'], email.from
+  ensure
+    PasswordMailer.domain = nil
+  end
+
+  def test_fully_changed
+    user = create_user
+    request = ActionDispatch::TestRequest.new
+    request.host = 'www.domain.com'
+
+    PasswordMailer.from_user = 'me'
+    PasswordMailer.domain = 'another.com'
+    PasswordMailer.default_url_options[:host] = request.host
+    email = PasswordMailer.password_reset_instructions user, request
+
+    assert_equal ['me@another.com'], email.from
+  ensure
+    PasswordMailer.from_user = 'no-reply'
+    PasswordMailer.domain = nil
   end
 
 end
